9c55934886a1fef3842fca73ae7fa977983694b2
Justineo <justice360@gmail.com>
2021-02-02 17:57:29 +0800

    feat: update hm code

--- plugins/hm.js
+++ plugins/hm.js
@@ -1,10 +1,10 @@
 export default ({ app }) => {
   if (process.env.NODE_ENV !== 'production') return
 
-  var _hmt = window._hmt = window._hmt || []
+  var _hmt = (window._hmt = window._hmt || [])
 
   var hm = document.createElement('script')
-  hm.src = 'https://hm.baidu.com/hm.js?d204d9e2f2ad504d6fe10939a9b5b588'
+  hm.src = 'https://hm.baidu.com/hm.js?3b91e2b87603771f136a8c593a515811'
   var s = document.getElementsByTagName('script')[0]
   s.parentNode.insertBefore(hm, s)
 
