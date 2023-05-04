af255b6542a9e50ed0ad99c58b92676b889a716e
Justineo <justice360@gmail.com>
2023-05-04 20:28:27 +0800

    feat: improve preload

--- plugins/preload.js
+++ plugins/preload.js
@@ -1,2 +1,22 @@
+const queued = {
+  live: false
+}
+
 const queue = requestIdleCallback || setTimeout
-queue(() => import('../components/OneLive'))
+
+let mql = window.matchMedia('(max-width: 480px)')
+
+mql.addEventListener('change', ({ matches }) => {
+  loadLive(matches)
+})
+
+loadLive(mql.matches)
+
+function loadLive (isNarrow) {
+  if (queued.live || isNarrow) {
+    return
+  }
+
+  queued.live = true
+  queue(() => import('../components/OneLive'))
+}
